353f896b14fd15e65b5fe0a2238dd4b1cf42145d
Japr07 <japr0701@gmail.com>
2018-09-26 15:33:50 -0400

    agreado botones para añadir precuela/secuela

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 const generarHTML = document.getElementById("generarHTML");
+
+
+/*//////////////
+///Imagenes/////
+//////////////*/
+
 let imagenes = 1;
 let imgCount = 0;
-let urls = 1;
 let imgForm = "";
-let urlForm = "";
 let imgHTML = "";
-let urlHTML = "";
 
 $("#imgBtn").click(function (e) {
     e.preventDefault();
     imgForm = "";
-    imagenes++;
     let imgSave = new Array();
     let posicionImg;
 
@@ -20,6 +22,7 @@ $("#imgBtn").click(function (e) {
     }
 
     imgCount++
+    imagenes++;
 
     for (let i = 1; i < imagenes; i++) {
         imgForm += `
@@ -42,6 +45,66 @@ $("#imgBtn").click(function (e) {
 
     }
 });
+
+const borrarImg = $(".borrarInputImg");
+
+$(borrarImg).on("click", ".borrarImg", function (e) {
+    e.preventDefault();
+    imgForm = "";
+    imagenes--;
+    let imgSave = new Array();
+    let posicionImg;
+
+    for (let i = 1; i <= imgCount; i++) {
+        posicionImg = document.getElementById(`imagenes${[i]}`).value;
+        imgSave.push(posicionImg);
+    }
+    imgSave.splice(imgSave.indexOf($(this).parent().prev().val()), 1);
+    imgCount--;
+
+
+    $(this).parent().parent().remove();
+
+    for (let i = 1; i < imagenes; i++) {
+        imgForm += `
+    <div class="input-group mt-2">
+        <div class="input-group-prepend">
+            <span class="input-group-text">Imagen ${[i+1]}:</span>
+        </div>
+        <input type="text" id="imagenes${[i]}" class="form-control">
+        <div class="input-group-append">
+            <input type="button" value="Eliminar" class="btn btn-danger borrarImg">
+        </div>
+    </div>
+    `
+    }
+
+    $("#imgExtra").html(imgForm);
+
+    for (let i = 0; i < imgCount; i++) {
+        document.getElementById(`imagenes${[i+1]}`).value = imgSave[i];
+    }
+})
+
+function mostrarImgHTML() {
+    let $imagenes = new Array();
+    imgHTML = "";
+
+    for (let i = 0; i < imagenes; i++) {
+        $imagenes.push(document.getElementById(`imagenes${[i]}`));
+        imgHTML += `
+        <img src="${$imagenes[i].value}">
+        `;
+    }
+};
+
+/*//////////////
+///////URL//////
+//////////////*/
+
+let urls = 1;
+let urlForm = "";
+let urlHTML = "";
 
 $("#urlBtn").click(function (e) {
     e.preventDefault();
@@ -82,48 +145,188 @@ $(borrarUrl).on("click", ".borrarUrl", function (e) {
     `
     }
     $("#urlExtra").html(urlForm);
-})
+});
 
-const borrarImg = $(".borrarInputImg");
+function mostrarUrlHTML() {
+    let $urlDescarga = new Array();
+    let $urlNombre = new Array();
+    let $urlIcono = new Array();
 
-$(borrarImg).on("click", ".borrarImg", function (e) {
+    urlHTML = "";
+
+    for (let i = 0; i < urls; i++) {
+        $urlDescarga.push(document.getElementById(`urlDescarga${[i]}`));
+        $urlNombre.push(document.getElementById(`urlNombre${[i]}`));
+        $urlIcono.push(document.getElementById(`urlIcono${[i]}`));
+        urlHTML += `
+        <a class="btn bg-blue-violet" href="${$urlDescarga[i].value}" target="_blank">${$urlIcono[i].value} ${$urlNombre[i].value}</a>        
+        `;
+    }
+};
+
+/*//////////////
+/////PreSec/////
+//////////////*/
+
+let preSec = false;
+
+function mostrarPreSecHtml() {
+    if (preSec) {
+        let $preNombre = new Array();
+        let $preDireccion = new Array();
+        let $secNombre = new Array();
+        let $secDireccion = new Array();
+
+        preHTML = "";
+        secHTML = "";
+
+        if (preCount > 0) {
+            for (let i = 0; i < preCount; i++) {
+                $preNombre.push(document.getElementById(`preNombre${[i]}`));
+                $preDireccion.push(document.getElementById(`preDescarga${[i]}`));
+                preHTML += `
+                <div class="related-links"><span><b>Precuela</b></span><span><a href="${$preDireccion[i].value}">${$preNombre[i].value}</a></span></div>
+                `;
+            }
+        }
+        if (secCount > 0) {
+            for (let i = 0; i < secCount; i++) {
+                $secNombre.push(document.getElementById(`secNombre${[i]}`));
+                $secDireccion.push(document.getElementById(`secDescarga${[i]}`));
+                preHTML += `
+                <div class="related-links"><span><b>Secuela</b></span><span><a href="${$secDireccion[i].value}">${$secNombre[i].value}</a></span></div>
+                `;
+            }
+        }
+    } else {
+        preHTML = "";
+        secHTML = "";
+    }
+};
+
+/*//////////////
+///Precuelas////
+//////////////*/
+
+let preCount = 0;
+let preForm = "";
+let preHTML = "";
+
+$("#preBtn").click(function (e) {
     e.preventDefault();
-    imgForm = "";
-    imagenes--;
-    let imgSave = new Array();
-    let posicionImg;
-
-    for (let i = 1; i <= imgCount; i++) {
-        posicionImg = document.getElementById(`imagenes${[i]}`).value;
-        imgSave.push(posicionImg);
+    preSec = true;
+    preForm = "";
+    preCount++;
+    for (let i = 0; i < preCount; i++) {
+        preForm += `
+        <div class="input-group mb-2">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Precuela:</span>
+            </div>
+            <input type="text" placeholder="Nombre" id="preNombre${[i]}" class="form-control">
+            <input type="text" placeholder="Direccion" id="preDescarga${[i]}" class="form-control">
+            <div class="input-group-append">
+                <input type="button" value="Eliminar" class="btn btn-danger borrarPre">
+            </div>
+        </div>
+       `
     }
-    imgSave.splice(imgSave.indexOf($(this).parent().prev().val()),1);
-    imgCount--;
+    $("#pre").html(preForm);
+});
 
+const borrarPre = $(".borrarInputPre");
 
+$(borrarPre).on("click", ".borrarPre", function (e) {
+    e.preventDefault();
     $(this).parent().parent().remove();
-
-    for (let i = 1; i < imagenes; i++) {
-        imgForm += `
-    <div class="input-group mt-2">
-        <div class="input-group-prepend">
-            <span class="input-group-text">Imagen ${[i+1]}:</span>
+    preForm = "";
+    preCount--;
+    for (let i = 0; i < preCount; i++) {
+        preForm += `
+        <div class="input-group mb-2">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Precuela:</span>
+            </div>
+            <input type="text" placeholder="Nombre" id="preNombre${[i]}" class="form-control">
+            <input type="text" placeholder="Direccion" id="preDescarga${[i]}" class="form-control">
+            <div class="input-group-append">
+                <input type="button" value="Eliminar" class="btn btn-danger borrarPre">
+            </div>
         </div>
-        <input type="text" id="imagenes${[i]}" class="form-control">
-        <div class="input-group-append">
-            <input type="button" value="Eliminar" class="btn btn-danger borrarImg">
-        </div>
-    </div>
-    `
+       `
+    }
+    
+    if (preCount == 0 && secCount == 0) {
+        preSec = false;
     }
 
-    $("#imgExtra").html(imgForm);
-
-    for (let i = 0; i < imgCount; i++) {
-        document.getElementById(`imagenes${[i+1]}`).value = imgSave[i];
-
-    }
+    $("#pre").html(preForm);
 })
+
+/*//////////////
+////Secuelas////
+//////////////*/
+
+
+let secCount = 0;
+let secForm = "";
+let secHTML = "";
+
+
+$("#secBtn").click(function (e) {
+    e.preventDefault();
+    preSec = true;
+    secForm = "";
+    secCount++;
+    for (let i = 0; i < secCount; i++) {
+        secForm += `
+        <div class="input-group mb-2">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Secuela:</span>
+            </div>
+            <input type="text" placeholder="Nombre" id="secNombre${[i]}" class="form-control">
+            <input type="text" placeholder="Direccion" id="secDescarga${[i]}" class="form-control">
+            <div class="input-group-append">
+                <input type="button" value="Eliminar" class="btn btn-danger borrarSec">
+            </div>
+        </div>
+       `
+    }
+    $("#sec").html(secForm);
+});
+
+const borrarSec = $(".borrarInputSec");
+
+$(borrarSec).on("click", ".borrarSec", function (e) {
+    e.preventDefault();
+    $(this).parent().parent().remove();
+    secForm = "";
+    secCount--;
+    for (let i = 0; i < secCount; i++) {
+        secForm += `
+        <div class="input-group mb-2">
+            <div class="input-group-prepend">
+                <span class="input-group-text">Secuela:</span>
+            </div>
+            <input type="text" placeholder="Nombre" id="secNombre${[i]}" class="form-control">
+            <input type="text" placeholder="Direccion" id="secDescarga${[i]}" class="form-control">
+            <div class="input-group-append">
+                <input type="button" value="Eliminar" class="btn btn-danger borrarSec">
+            </div>
+        </div>
+       `
+    }
+    
+    if (preCount == 0 && secCount == 0) {
+        preSec = false;
+    }
+
+    $("#sec").html(secForm);
+})
+
+/*//////////////
+//Generar HTML//
+//////////////*/
 
 generarHTML.addEventListener("click", function () {
     const nombreEntrada = document.getElementById("nombreEntrada");
@@ -140,30 +343,10 @@ generarHTML.addEventListener("click", function () {
     let audio = document.getElementsByClassName("audio")
     let subtitulo = document.getElementsByClassName("subtitulo");
     let formato = document.getElementsByClassName("formato");
-    let $imagenes = new Array();
-    let $urlDescarga = new Array();
-    let $urlNombre = new Array();
-    let $urlIcono = new Array();
 
-    urlHTML = "";
-
-    for (let i = 0; i < urls; i++) {
-        $urlDescarga.push(document.getElementById(`urlDescarga${[i]}`));
-        $urlNombre.push(document.getElementById(`urlNombre${[i]}`));
-        $urlIcono.push(document.getElementById(`urlIcono${[i]}`));
-        urlHTML += `
-        <a class="btn bg-blue-violet" href="${$urlDescarga[i].value}" target="_blank">${$urlIcono[i].value} ${$urlNombre[i].value}</a>        
-        `;
-    }
-
-    imgHTML = "";
-
-    for (let i = 0; i < imagenes; i++) {
-        $imagenes.push(document.getElementById(`imagenes${[i]}`));
-        imgHTML += `
-        <img src="${$imagenes[i].value}">
-        `;
-    }
+    mostrarPreSecHtml();
+    mostrarUrlHTML();
+    mostrarImgHTML();
 
     for (var i = 0; i < formato.length; i++) {
         if (formato[i].checked) {
@@ -200,6 +383,8 @@ generarHTML.addEventListener("click", function () {
         <div class="altpost__info">
             <div class="info__content">
                 <a>${textoEntrada.value}</a>
+                ${preHTML}
+                ${secHTML}
             </div>
         </div>
     </div>
@@ -226,8 +411,8 @@ generarHTML.addEventListener("click", function () {
                 Audio: ${audio}<br/>
                 Subtítulos: ${subtitulo}<br/>
                 Formato: ${formato}<br/>
-                Tamaño: ${peso.value}MB Aprox<br/>
-                Duración: ${duracion.value} min<br/>
+                Tamaño: ${peso.value}<br/>
+                Duración: ${duracion.value}<br/>
                 Calidad: ${calidad.value}<br/>
             </div>
         </div>
